0ce94a89a77958bf6cfa5403ba6a6011ebd33fb8
klgd <slice1213@gmail.com>
2021-01-08 15:32:26 +0800

    fix: 修复composer 2 报错
    修复composer2报` Failed to extract topthink/framework: (9) unzip -qq` 错误

--- src/ThinkFramework.php
+++ src/ThinkFramework.php
@@ -5,16 +5,29 @@ namespace think\composer;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
 use Composer\Repository\InstalledRepositoryInterface;
+use React\Promise\PromiseInterface;
 
 class ThinkFramework extends LibraryInstaller
 {
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        parent::install($repo, $package);
-        if ($this->composer->getPackage()->getType() == 'project' && $package->getInstallationSource() != 'source') {
-            //remove tests dir
-            $this->filesystem->removeDirectory($this->getInstallPath($package) . DIRECTORY_SEPARATOR . 'tests');
+        $afterInstall = function () use ($package) {
+            if ($this->composer->getPackage()->getType() == 'project' && $package->getInstallationSource() != 'source') {
+                //remove tests dir
+                $this->filesystem->removeDirectory($this->getInstallPath($package) . DIRECTORY_SEPARATOR . 'tests');
+            }
+        };
+
+        // install the package the normal composer way
+        $promise = parent::install($repo, $package);
+
+        // Composer v2 might return a promise here
+        if ($promise instanceof PromiseInterface) {
+            return $promise->then($afterInstall);
         }
+
+        // If not, execute the code right away as parent::install executed synchronously (composer v1, or v2 without async)
+        $afterInstall();
     }
 
     /**
@@ -42,11 +55,23 @@ class ThinkFramework extends LibraryInstaller
 
     public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
     {
-        parent::update($repo, $initial, $target);
-        if ($this->composer->getPackage()->getType() == 'project' && $target->getInstallationSource() != 'source') {
-            //remove tests dir
-            $this->filesystem->removeDirectory($this->getInstallPath($target) . DIRECTORY_SEPARATOR . 'tests');
+        $afterUpdate = function () use ($initial, $target) {
+            if ($this->composer->getPackage()->getType() == 'project' && $target->getInstallationSource() != 'source') {
+                //remove tests dir
+                $this->filesystem->removeDirectory($this->getInstallPath($target) . DIRECTORY_SEPARATOR . 'tests');
+            }
+        };
+
+        // update the package the normal composer way
+        $promise = parent::update($repo, $initial, $target);
+
+        // Composer v2 might return a promise here
+        if ($promise instanceof PromiseInterface) {
+            return $promise->then($afterUpdate);
         }
+
+        // If not, execute the code right away as parent::update executed synchronously (composer v1, or v2 without async)
+        $afterUpdate();
     }
 
     /**
